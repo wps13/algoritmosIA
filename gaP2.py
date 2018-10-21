cbc5d8b57320ffff1513b4fb681dfa9e2b6b2d95
wps13 <willanepaiva@hotmail.com>
2018-10-21 00:29:33 -0300

    Initialize selection Function

--- gaP2.py
+++ gaP2.py
@@ -1,5 +1,5 @@
 import numpy
-from random import randint
+from random import uniform
 
 '''
 Questão 3 - Prova 2
@@ -28,15 +28,38 @@ def fit():
 	sumFits = 1/sumFits
 	return sumFits
 
+fits=fit()
+
 def selecao():
 	totalFits = fits.sum()
-	print(totalFits)
+	#Armazena os indices dos cromossomos escolhidos
+	popAux = numpy.zeros(3)
+	aux = numpy.zeros(5)
+	#Armazena os limites para gerar roleta
+	valAux = 0
+	for i in range(5):
+		valAux += fits[i]
+		aux[i] = valAux
+	#Intervalos de possíveis valores
+	intInferior = 0
+	intSuperior = totalFits
+	#limites usados para encontrar cromossomo
 	limInferior = 0
-	limSuperior = totalFits
+	limSuperior = aux[0]
+	ind=0
 	while(ind <= 3):
-		indAtual = 
+		probab =uniform(intInferior,intSuperior)
+		for i in range(4):
+			if probab>limInferior and probab<=limInferior:
+				popAux= numpy.append(popAux,[i])
+				ind+=1
+			else:
+				limInferior=limSuperior
+				limSuperior=aux[i+1]
+	print(aux)
+	print(popAux)			 
 
-	
+selecao()	
 #def crossingOver():
 		
 
@@ -45,9 +68,4 @@ def selecao():
 Modifica o gene com maior peso, tornando-o um
 caminho não acessavel,por defini-lo como peso 0.
 Escolhe os cromossomos com maior fit.
-'''			
-	
-def main():
-	fits=fit()
-	selecao()
-	#crossingOver()
+'''
