6f8f5dfc7579b0a8d5bb0f47f607859dac3f6ea0
wps13 <willanepaiva@hotmail.com>
2018-10-21 09:28:01 -0300

    End Selection and Start Crossing Over

--- gaP2.py
+++ gaP2.py
@@ -34,6 +34,8 @@ def selecao():
 	totalFits = fits.sum()
 	#Armazena os indices dos cromossomos escolhidos
 	popAux = numpy.zeros(3)
+	novaPop = numpy.zeros((5,5))
+
 	aux = numpy.zeros(5)
 	#Armazena os limites para gerar roleta
 	valAux = 0
@@ -46,23 +48,37 @@ def selecao():
 	#limites usados para encontrar cromossomo
 	limInferior = 0
 	limSuperior = aux[0]
+	#variavel para controlar o preenchimeto da população
 	ind=0
-	while(ind <= 3):
+	while(ind < 3):
+		#Gera-se um valor para procurar na roleta
 		probab =uniform(intInferior,intSuperior)
+		#Procura-se o elemento entre os intervalos
+		#Tendo-se sido somado os fits do elemento atual com os anteriores para assim obter-se as faixas correspondentes
 		for i in range(4):
-			if probab>limInferior and probab<=limInferior:
-				popAux= numpy.append(popAux,[i])
+		#Caso o elemento esteja na faixa, armazena-se o indice
+		# correspondente ao cromossomo
+			if probab>=limInferior and probab<limInferior:
+				popAux[ind]=i
 				ind+=1
 			else:
 				limInferior=limSuperior
 				limSuperior=aux[i+1]
-	print(aux)
-	print(popAux)			 
+	#Armazena-se os genes escolhidos
+	for i in range(3):
+		novaPop[i]=pop[popAux[i]]			 
+	#Substitui a população atual pela nova
+	for i in range(5):
+		pop[i]=novaPop[i]
 
 selecao()	
-#def crossingOver():
-		
 
+def crossingOver():
+	#Matriz para armazenar os filhos
+	filhos = numpy.zeros((2,5))
+	for i in range(3):
+		for i in range(5):				
+			filhos
 #def mutacao():
 '''
 Modifica o gene com maior peso, tornando-o um
