8e081f37fbb2e6deb98103a24d654ef6bc62ef6c
wps13 <willanepaiva@hotmail.com>
2018-10-20 23:25:19 -0300

    modify population matrix

--- gaP2.py
+++ gaP2.py
@@ -7,48 +7,47 @@ Questão 3 - Prova 2
 Implementar algoritmo genético responsável por achar melhor caminho
 '''
 #cria população inicial
-pop = numpy.zeros(shape=(5,5))
-#matriz para armazenar os fits de cada cromossomo
-fits= numpy.zeros(shape=(5,1))
+pop = [
+	[0,2,9,3,6],
+	[2,0,4,3,8],
+	[9,4,0,7,3],
+	[3,3,7,0,3],
+	[6,8,3,3,0]]
 
-#preenchendo os pesos iniciais dos caminhos	
-pop[0][0] = 0
-pop[0][1] = 2
-pop[0][2] = 9
-pop[0][3] = 3
-pop[0][4] = 6
-
-pop[1][0] = 2
-pop[1][1] = 0
-pop[1][2] = 4
-pop[1][3] = 3
-pop[1][4] = 8
-
-pop[2][0] = 9
-pop[2][1] = 4
-pop[2][2] = 0
-pop[2][3] = 7
-pop[2][4] = 3
-
-pop[3][0] = 3
-pop[3][1] = 3
-pop[3][2] = 7
-pop[3][3] = 0
-pop[3][4] = 3
-	
-pop[4][0] = 6
-pop[4][1] = 8
-pop[4][2] = 3
-pop[4][3] = 3
-pop[4][4] = 0
-	
-for i in pop:
-	print(i)
+#array para armazenar os fits de cada cromossomo
+fits=numpy.zeros(5)
 
 def fit():
+	'''
+	Função que calcula os fits dos cromossomos
+	Quanto maior o valor, melhor é o cromossomo
+	soma-se os pesos dos genes e calcula o inverso
+	Visto que procura-se o menor caminho possível
+	'''
+	sumFits=numpy.sum(pop,axis=1)
+	sumFits = 1/sumFits
+	return sumFits
 
-fit()
+def selecao():
+	totalFits = fits.sum()
+	print(totalFits)
+	limInferior = 0
+	limSuperior = totalFits
+	while(ind <= 3):
+		indAtual = 
 
-for i in fits:
-	print(i)
+	
+#def crossingOver():
+		
 
+#def mutacao():
+'''
+Modifica o gene com maior peso, tornando-o um
+caminho não acessavel,por defini-lo como peso 0.
+Escolhe os cromossomos com maior fit.
+'''			
+	
+def main():
+	fits=fit()
+	selecao()
+	#crossingOver()
